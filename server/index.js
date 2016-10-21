27ca14bf6266f480ccb1a219fe0e4c4778f4bd63
saguas <luisfmfernandes@gmail.com>
2016-10-21 10:39:52 +0100

    Fix import updateuser.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ import { Reaction } from "/server/api";
 import "./connect/login.js";
 import "./user/createUser.js";
 import "./user/methods.js";
-
+import "./user/updateUser.js";
 
 //Object.assign(Reaction.Collections, Collections);
-
